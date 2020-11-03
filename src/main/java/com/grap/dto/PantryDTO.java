fea5e34c220a252d152f384b873201a7022b42e0
Parneet1712 <60361272+Parneet1712@users.noreply.github.com>
2020-11-02 19:34:49 -0500

    Created Pantry DTO

--- src/main/java/com/grap/dto/PantryDTO.java
+++ src/main/java/com/grap/dto/PantryDTO.java
@@ -1,7 +1,17 @@
 package com.grap.dto;
-import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-public class PantryDTO{
+public class PantryDTO {
 
+    @SerializedName("ingredients")
+    private String ingredients;
+
+    @SerializedName("quantity")
+    private int quantity;
+
+    @SerializedName("name")
+    private String name;
+
+    @SerializedName("measurement")
+    private Double measurement;
 }
