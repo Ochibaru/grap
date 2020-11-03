28d0aa0efa817f6745e1831b25e36e55980998aa
musaevaa <musaevaa@mail.uc.edu>
2020-11-02 19:26:54 -0500

    Created Profile DTO

--- src/main/java/com/grap/dto/ProfileDTO.java
+++ src/main/java/com/grap/dto/ProfileDTO.java
@@ -1,7 +1,19 @@
 package com.grap.dto;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+import org.springframework.lang.NonNull;
 
 public class ProfileDTO{
+
+    @NonNull
+    private String firstName;
+    @NonNull
+    private String lastName;
+    @NonNull
+    private String username;
+    @NonNull
+    private String email;
+    @NonNull
+    private String address;
 
 }
