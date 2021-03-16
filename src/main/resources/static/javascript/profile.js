adc9068e7d6a9e724e448dc49c6669c60b6eb5b7
musaevaa <musaevaa@mail.uc.edu>
2021-03-16 11:53:30 -0400

    Profile photo changes

--- src/main/resources/static/javascript/profile.js
+++ src/main/resources/static/javascript/profile.js
@@ -145,34 +145,4 @@ function signOut() {
         })
     });
 
-    /**
-     * Initializes the app.
-     */
-    var initApp = function () {
-        document.getElementById('return').addEventListener('click', function () {
-            window.location.replace("userHome=" + authResult.user.uid);
-        });
-    };
-
-    window.addEventListener('load', initApp);
-    $(document).ready(function () {
-
-
-        var readURL = function (input) {
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
-
-                reader.onload = function (e) {
-                    $('.avatar').attr('src', e.target.result);
-                }
-
-                reader.readAsDataURL(input.files[0]);
-            }
-        }
-
-
-        $(".file-upload").on('change', function () {
-            readURL(this);
-        });
-    });
 }
